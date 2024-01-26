04d3b30d6c2907df62fdd122fe3985327fcb1eaa
basile.trebus-hamann@epitech.eu <administrateur@gip-fcip-alsace.fr>
2024-01-26 15:22:29 +0100

    add: render grid

---
+++ exemples/verlet/src/systems/renderGrid.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "../components/components.hpp"
+
+#include "ypi/src/core/ecs/system/system.hpp"
+#include "ypi/src/core/ecs/coordinator.hpp"
+#include "ypi/src/core/window/Window.hpp"
+
+#include "ypi/lib_headers/entt.hpp"
+
+namespace sys
+{
+
+    class RenderGrid
+    {
+        public:
+            RenderGrid(unsigned int width, unsigned int height, float cellSize)
+            {
+                m_vertexBuffer.setPrimitiveType(sf::Lines);
+                m_vertexBuffer.create(width * height * 2);
+
+                for (unsigned int i(0); i < width; ++i) {
+                    for (unsigned int j(0); j < height; ++j) {
+                        m_vertices.push_back(sf::Vertex());
+                        m_vertices.push_back(sf::Vertex());
+                        m_vertices.push_back(sf::Vertex());
+                        m_vertices.push_back(sf::Vertex());
+
+                        m_vertices.back().position = {i * cellSize, j * cellSize};
+                        m_vertices[m_vertices.size() - 2].position = {(i + 1) * cellSize, j * cellSize};
+                        m_vertices[m_vertices.size() - 3].position = {(i + 1) * cellSize, j * cellSize};
+                        m_vertices[m_vertices.size() - 4].position = {(i + 1) * cellSize, (j + 1) * cellSize};
+
+                        m_vertices.back().color = sf::Color::White;
+                        m_vertices[m_vertices.size() - 2].color = sf::Color::White;
+                        m_vertices[m_vertices.size() - 3].color = sf::Color::White;
+                        m_vertices[m_vertices.size() - 4].color = sf::Color::White;
+
+                        m_vertices.back().color.a = 50;
+                        m_vertices[m_vertices.size() - 2].color.a = 50;
+                        m_vertices[m_vertices.size() - 3].color.a = 50;
+                        m_vertices[m_vertices.size() - 4].color.a = 50;
+                    }
+                }
+
+                m_vertexBuffer.update(m_vertices.data());
+            }
+
+            void render(float offsetX, float offsetY, exng::Window &window)
+            {
+                sf::RenderStates states;
+                states.transform.translate(offsetX, offsetY);
+                window.draw(m_vertexBuffer, states);
+            }
+
+        private:
+            sf::VertexBuffer m_vertexBuffer;
+            std::vector<sf::Vertex> m_vertices;
+    };
+
+}
